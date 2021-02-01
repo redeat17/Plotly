93c6270a5f47197fd49d5822fa6b1357477870e5
Redeat Bekele <redeat17@gmail.com>
2021-01-31 20:14:06 -0500

    belly app

--- app.py
+++ app.py
@@ -6,17 +6,14 @@ app = Flask(__name__)
 @app.route("/")
 def index():
 
-    # use render_template to serve up the index.html
-    return # your-code-here
+    return render_template('index.html')
 
 @app.route("/samples")
 def samples():
-
-    # open the json file, located at static/data/samples.json
-    # use json.load() to read in the file as json
-    # return that json through the Flask endpoini
     
-    return # your-code-here
+    with open('static/data/samples.json') as json_file:
+
+        return json.load(json_file)
 
 if __name__ == "__main__":
     app.run(debug=True)
--- static/js/app.js
+++ static/js/app.js
@@ -1,26 +1,72 @@
-function init() {
-  // Grab a reference to the dropdown select element
 
-  // Use the list of sample names to populate the select options
-  d3.json("/samples").then((data) => {
-    
-    // your-code-here
+showOptions();
+showData();
+optionChanged();
 
-    // Use the first sample from the list to build the initial plots
-  });
-}
+function showOptions() {
+    d3.json('/samples').then(data => {
 
-/*
-   Hints: Create additional functions to build the charts,
-          build the gauge chart, set up the metadata,
-          and handle the event listeners
+        var { names } = data;
+        names.forEach(name => {
+            d3.select('select').append('option').text(name);
+        });
+    });
+};
 
-   Recommended function names:
-    optionChanged() 
-    buildChart()
-    buildGauge()
-    buildMetadata()
-*/
+function showData() {
+    d3.json('/samples').then(data => {
+        var { metadata, samples } = data;
+        var selection = d3.select('select').property('value');
 
-// Initialize the dashboard
-init();
+        d3.select('.panel-body').html('');
+        metadata = metadata.filter(obj => obj.id == selection)[0];
+        Object.entries(metadata).forEach(([key, val]) => {
+            d3.select('.panel-body').append('h5').text(`${key.toUpperCase()}: ${val}`);
+        });
+
+        samples = samples.filter(obj => obj.id == selection)[0];
+        var { otu_ids, otu_labels, sample_values } = samples;
+
+        var barData = [{
+            x: sample_values.slice(0,10).reverse(),
+            y: otu_ids.slice(0,10).reverse().map(id => `OTU ${id}`),
+            text: otu_labels.slice(0,10).reverse(),
+            type: 'bar',
+            orientation: 'h'
+        }];
+
+        Plotly.newPlot('bar', barData);
+
+        var bubbleData = [{
+            x: otu_ids,
+            y: sample_values,
+            text: otu_labels,
+            mode: 'markers',
+            marker: {
+                color: otu_ids,
+                size: sample_values
+            }
+        }];
+
+        Plotly.newPlot('bubble', bubbleData);
+
+        var gaugeData = [
+            {
+                domain: { x: [0, 1], y: [0, 1] },
+                value: metadata.wfreq,
+                title: { text: "Belly Button Wash Frequency <br> Scrubs per Week" },
+                type: "indicator",
+                mode: "gauge+number",
+                delta: { reference: 400 },
+                gauge: { axis: { range: [0, 9] } }
+            }
+        ];
+
+        var layout = { width: 600, height: 400 };
+        Plotly.newPlot('gauge', gaugeData, layout);
+    });
+};
+
+function optionChanged() {
+    showData();
+};
